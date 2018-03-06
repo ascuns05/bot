0916a9bb687f770fe46775ac82d942187c8e07c5
freethinkel <freethinkel@Air.local>
2018-03-06 21:26:54 +0300

    fix

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
