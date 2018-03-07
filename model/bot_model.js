cd34ec3355311af8ef5855b88f62ca3476e3113a
freethinkel <freethinkel@Air.local>
2018-03-07 13:31:48 +0300

    add send shedule

--- model/bot_model.js
+++ model/bot_model.js
@@ -1,8 +1,8 @@
 let TelegramBot = require('node-telegram-bot-api')
 , token = '384921691:AAEPgUNJGCazCM0YcftGwIVHokl1c7jpygE'
 , shedule = require('../model/shedule')
+, fs = require('fs')
 ;
-
 
 var bot = new TelegramBot(token, {polling: true});
 let data = {};
@@ -10,15 +10,25 @@ let data = {};
 bot.onText(/\/start/, msg => {
   console.log(msg);
   data[msg.from.id] = msg.from;
-  let text = shedule.odd[0].join('\n');
+  let date = new Date();
+  let text = shedule.odd[date.getDay()].join('\n');
   bot.sendMessage(msg.chat.id, text );
 });
 
-setInterval(sendShelude, 5*60000);
+let isSend = false;
+setInterval(sendShelude, 1000);
 
 function sendShelude() {
-  let date = new Date();
-
+  let date = new Date();  
+  let text = shedule.odd[date.getDay()].join('\n');
+  if (date.getHours() === 13 && !isSend) {
+    for (key in data) {
+      bot.sendMessage(key, text);
+    }
+    isSend = !isSend;
+  }
 }
+
+
 
 module.exports = data;
