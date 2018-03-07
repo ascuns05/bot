6303824f2366071f4b8f55b0667d373425c5cd11
freethinkel <freethinkel@Air.local>
2018-03-07 11:32:36 +0300

    add a shelude

--- model/bot_model.js
+++ model/bot_model.js
@@ -1,18 +1,24 @@
 let TelegramBot = require('node-telegram-bot-api')
 , token = '384921691:AAEPgUNJGCazCM0YcftGwIVHokl1c7jpygE'
+, shedule = require('../model/shedule')
 ;
 
+
 var bot = new TelegramBot(token, {polling: true});
+let data = [];
 
-bot.onText('/\/echo (.+)/', function (msg, match) {
-    let fromId = msg.from.id;
-    let resp = match[1];
-    bot.sendMessage(fromId, resp);
-    console.log(msg);
+bot.onText(/\/start/, msg => {
+  console.log(msg);
+  data.push( msg.from);
+  let text = shedule[1].join('\n');
+  bot.sendMessage(msg.chat.id, text );
 });
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Бот в процессе создания');
-});
+setInterval(sendShelude, 5*60000);
+
+function sendShelude() {
+  let date = new Date();
+  
+}
+
+module.exports = data;
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+let botData = require('../model/bot_model');
 
 let data = {
-  title: 'Dashboard'
-}
+  title: 'ads',
+  data: botData
+};
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('dashboard', data);
+  console.log(data.data)
 });
 
 module.exports = router;
