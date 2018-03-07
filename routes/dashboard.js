dca7e1de0ae981616ef1c19174b0d057e6dd788d
freethinkel <freethinkel@Air.local>
2018-03-07 23:01:56 +0300

    fix users view

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,21 +1,17 @@
 let express = require('express')
 , router = express.Router()
 , botData = require('../model/bot_model')
-, userCollection = require('../model/crud').allRead()
 ;
 
-userCollection.then( (data) => { 
-  router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
+  let userCollection = require('../model/crud').allRead();
+  userCollection.then(data => {
+    console.log(data);
+    
     res.render('dashboard', { data });
-    console.log(data[0].id)
-  });
+  } );
+
 });
 
-
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.render('dashboard', data);
-//   console.log(data.length)
-// });
 
 module.exports = router;
