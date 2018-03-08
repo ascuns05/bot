c7175a51f0f3360143afaa8e5090a5278e038959
freethinkel <freethinkel@Air.local>
2018-03-08 12:32:41 +0300

    test heroku auto deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shedule Bot for PM-1-16' });
+  res.render('index', { title: 'Shedule Bot for PM-1-16 Test' });
 });
 
 module.exports = router;
